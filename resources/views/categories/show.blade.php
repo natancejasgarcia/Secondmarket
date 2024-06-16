6cd640bc58fc33d9948da6f13a3076d38faf15ec
natancejasgarcia <ncejgar376@g.educaand.es>
2024-06-16 12:18:23 +0200

    categ pro 2

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -54,7 +54,7 @@
         @if ($category->products->isEmpty())
         <div class="no-products">
             <p>No hay productos disponibles en esta categoría.</p>
-            <img src="https://via.placeholder.com/600x400?text=No+Products+Available" alt="No products available">
+            <img class="padding-bottom:50px;" src="https://via.placeholder.com/600x400?text=No+Products+Available" alt="No products available">
         </div>
         @else
         <div class="row row-cols-1 row-cols-md-3">
